4293ea0a9c46a34dc854b7a70a3e60f4be9419da
Giorgio <giorgio.pignattari@gmail.it>
2021-05-20 17:50:13 +0200

    collegato controller a index.blade

--- laravel-base-crud/app/Http/Controllers/ComicController.php
+++ laravel-base-crud/app/Http/Controllers/ComicController.php
@@ -14,7 +14,9 @@ class ComicController extends Controller
      */
     public function index()
     {
-        //
+      $comics = Comic::all();
+
+      return view('comics.index', compact('comics'));
     }
 
     /**
