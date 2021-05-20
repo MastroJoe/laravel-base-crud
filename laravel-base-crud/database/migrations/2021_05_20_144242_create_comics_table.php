32cff73b2b0eff98e1e12a42d26c397803f94c07
Giorgio <giorgio.pignattari@gmail.it>
2021-05-20 17:07:58 +0200

    compilazione file migration e creazione seeder

--- laravel-base-crud/database/migrations/2021_05_20_144242_create_comics_table.php
+++ laravel-base-crud/database/migrations/2021_05_20_144242_create_comics_table.php
@@ -15,6 +15,13 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->text('thumb')->nullable();
+            $table->tinyInteger('price');
+            $table->string('series');
+            $table->date('sale_date');
+            $table->tinyText('type');
             $table->timestamps();
         });
     }
