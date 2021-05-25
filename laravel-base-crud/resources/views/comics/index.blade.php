398fad1e58f58f522613aea0818ed7b04390bb6b
Giorgio <giorgio.pignattari@gmail.it>
2021-05-25 16:36:29 +0200

    comic.show

--- laravel-base-crud/resources/views/comics/index.blade.php
+++ laravel-base-crud/resources/views/comics/index.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.app')
 
 @section('main')
-{{ var_dump($comics) }}
+<main>
+  <div class="container-fluid card-container">
+    <!-- stampo tutte le cards con foreach -->
+    @foreach ($comics as $index => $comic)
+      <div class="cards">
+        <h4>{{ $comic->title }}</h4>
+        <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+        <p>Serie: {{ $comic->series }}</p>
+        <p>Genere: {{ $comic->type }}</p>
+        <p>Prezzo: {{ $comic->price }} €</p>
+        <a href="{{ route('comics.show', ['comic' => $comic->id]) }}">
+          Info
+        </a>
+      </div>
+    @endforeach
+  </div>
+</main>
 @endsection
